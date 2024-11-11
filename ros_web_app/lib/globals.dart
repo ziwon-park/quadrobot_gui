092485ecac06f94024834a75370b3a1b85d3223c
ziwon-park <ziwon@kaist.ac.kr>
2024-11-11 17:17:43 +0900

    edit main.dart

---
+++ ros_web_app/lib/globals.dart
@@ -0,0 +1,6 @@
+library globals;
+
+String ip_address = "";
+String full_address = "";
+String rosbridge_port = "";
+String fastapi_port = "";
--- ros_web_app/lib/main.dart
+++ ros_web_app/lib/main.dart
@@ -1,130 +1,177 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:yaml/yaml.dart';
 
 import './pages/main_page.dart';
 import 'globals.dart' as globals;
 
-
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quadruped Robot',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.grey,
+        primaryColor: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.light(
+          primary: Colors.grey,
+          secondary: Colors.blue,
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: InitialPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class InitialPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _InitialPageState createState() => _InitialPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _InitialPageState extends State<InitialPage> {
+  dynamic configData;
+  String selectedRobot = 'Unitree Go1';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void onPressed(ipController) async {
+    final yamlString = await rootBundle.loadString('lib/assets/config.yaml');
+    final yamlDoc = loadYaml(yamlString);
+
+    String yamlIpAddress = yamlDoc['ip_address'];
+    globals.fastapi_port = yamlDoc['fastapi_port'];
+    globals.ip_address =  yamlIpAddress;
+
+    String fullAddress = "ws://$yamlIpAddress:${ipController.text}";
+
+    globals.full_address = fullAddress;
+    print("새로운 IP 주소가 저장되었습니다: ${globals.full_address}");
+
+    // Navigator.push(
+    //   context,
+    //   MaterialPageRoute(builder: (context) => MainPage()),
+    // );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    final screenHeight = MediaQuery.of(context).size.height;
+    final containerHeight = screenHeight * 0.8;
+    final TextEditingController ipController = TextEditingController();
+
+
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: SingleChildScrollView(
+          // physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 100.0),
+                child: Image.asset('/home/ubuntuurl/ziwon/gui_ws/src/ros_web_app/lib/assets/url_logo.png', height: 100),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 15.0),
+                // child: Text(
+                //   'for Robotic Experiments',
+                //   style: TextStyle(
+                //     fontSize: 15,
+                //     fontWeight: FontWeight.bold,
+                //     color: Colors.grey,
+                //   ),
+                // ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: containerHeight,
+                margin: EdgeInsets.all(0),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Column(
+                  children: [
+                    Image.asset('/home/ubuntuurl/ziwon/gui_ws/src/ros_web_app/lib/assets/unitree_go1.png', height: 200),
+                    SizedBox(height: 30),
+                    DropdownButtonFormField<String>(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40.0),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      value: selectedRobot,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedRobot = newValue!;
+                        });
+                      },
+                      items: <String>['Unitree Go1', 'Robot B2', 'Robot C3']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                    SizedBox(height: 24),
+                    TextField(
+                      controller: ipController,
+                      decoration: InputDecoration(
+                        hintText: 'Enter IP Address',
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue, width: 5.0),
+                          borderRadius: BorderRadius.circular(40.0),
+                        ),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                    SizedBox(height: 24),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 4, 68, 148)),
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40.0),
+                          ),
+                        ),
+                      ),
+                      onPressed: () => onPressed(ipController),
+                      child: Text('START',style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),),
+                    ),
+                    SizedBox(height: 24),
+                    Container(
+                      alignment: Alignment.bottomCenter,
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                      child: Text('@ Created by Jiwon Park, Urban Robotics Lab @ KAIST',
+                          style: TextStyle(fontSize: 11, color: Colors.black54)),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
