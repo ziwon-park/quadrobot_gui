534afbb803533bffab4bf586e633059cd04ec608
ziwon-park <ziwon@kaist.ac.kr>
2024-12-04 14:58:54 +0900

    Add velocity viewer

--- ros_web_app/lib/pages/controller.dart
+++ ros_web_app/lib/pages/controller.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:get/get.dart';
 import 'dart:async';
 import '../globals.dart' as globals;
+import 'package:fl_chart/fl_chart.dart';
 
 class JoystickController extends GetxController {
   var center = Offset(0, 0).obs;
@@ -13,12 +14,28 @@ class JoystickController extends GetxController {
   var maxLinVel = 0.1.obs;
   var maxRotVel = 0.05.obs;
 
+  var linearVelocities = <FlSpot>[].obs;
+  var angularVelocities = <FlSpot>[].obs;
+  var currentTime = 0.0.obs;
+
   updatePosition(DragUpdateDetails position) {
     movement.value = position.localPosition - center.value;
     double distance = movement.value.distance;
     if (distance > radius) {
       movement.value = movement.value * (radius / distance);
     }
+  }
+
+  void addVelocityData(double linear, double angular) {
+    final time = currentTime.value;
+    
+    linearVelocities.removeWhere((spot) => spot.x < time - 3.0);
+    angularVelocities.removeWhere((spot) => spot.x < time - 3.0);
+    
+    linearVelocities.add(FlSpot(time, linear));
+    angularVelocities.add(FlSpot(time, angular));
+    
+    currentTime.value += 0.1; 
   }
 }
 
@@ -31,6 +48,7 @@ class _ControllerBlockState extends State<ControllerBlock> {
   late RosService rosService;
   late JoystickController controller;
   Timer? timer;
+  Timer? plotTimer;
 
   @override
   void initState() {
@@ -38,6 +56,12 @@ class _ControllerBlockState extends State<ControllerBlock> {
     rosService = RosService(globals.full_address);
     controller = Get.put(JoystickController());
     _connectToRos();
+    
+    plotTimer = Timer.periodic(Duration(seconds: 1), (Timer t) {
+      if (mounted) {
+        setState(() {});
+      }
+    });
   }
 
   Future<void> _connectToRos() async {
@@ -48,6 +72,8 @@ class _ControllerBlockState extends State<ControllerBlock> {
   void sendCmd() {
     double linear = -controller.movement.value.dy / controller.radius * controller.maxLinVel.value;
     double angular = -controller.movement.value.dx / controller.radius * controller.maxRotVel.value;
+
+    controller.addVelocityData(linear, angular);
 
     var message = {
       'op': 'publish',
@@ -60,6 +86,44 @@ class _ControllerBlockState extends State<ControllerBlock> {
     rosService.publish(json.encode(message));
   }
 
+  Widget buildVelocityChart(String title, List<FlSpot> spots, Color color) {
+    return Container(
+      height: 200,
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Text(title),
+          Expanded(
+            child: LineChart(
+              LineChartData(
+                minY: -1.0,
+                maxY: 1.0,
+                minX: controller.currentTime.value - 3.0,
+                maxX: controller.currentTime.value,
+                gridData: FlGridData(show: true),
+                titlesData: FlTitlesData(
+                  rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                  topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                  bottomTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                ),
+                borderData: FlBorderData(show: true),
+                lineBarsData: [
+                  LineChartBarData(
+                    spots: spots,
+                    isCurved: true,
+                    color: color,
+                    dotData: FlDotData(show: false),
+                    belowBarData: BarAreaData(show: false),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   void resetValues() {
     controller.maxLinVel.value = 0.1;
     controller.maxRotVel.value = 0.05;
@@ -68,6 +132,7 @@ class _ControllerBlockState extends State<ControllerBlock> {
   @override
   void dispose() {
     timer?.cancel();
+    plotTimer?.cancel();
     rosService.disconnect();
     super.dispose();
   }
@@ -78,102 +143,131 @@ class _ControllerBlockState extends State<ControllerBlock> {
       elevation: 4.0,
       child: Container(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              'Robot Control',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+            Expanded(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Obx(() => buildVelocityChart(
+                        'Linear Velocity (m/s)',
+                        controller.linearVelocities,
+                        Colors.blue,
+                      )),
+                  SizedBox(height: 16),
+                  Obx(() => buildVelocityChart(
+                        'Angular Velocity (rad/s)',
+                        controller.angularVelocities,
+                        Colors.red,
+                      )),
+                ],
               ),
             ),
-            SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Obx(() => Text(
-                          '${controller.maxRotVel.value.toStringAsFixed(2)} rad/s',
-                          style: TextStyle(fontSize: 14),
-                        )),
-                    RotatedBox(
-                      quarterTurns: 3,
-                      child: Obx(() => Slider(
-                            value: controller.maxRotVel.value,
-                            min: 0,
-                            max: 1.0,
-                            divisions: 20,
-                            onChanged: (value) {
-                              controller.maxRotVel.value = value;
-                            },
-                          )),
+            Expanded(
+              flex: 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Robot Control',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
-                GestureDetector(
-                  onPanStart: (details) {
-                    controller.center.value = details.localPosition;
-                    controller.movement.value = Offset(0, 0);
-                  },
-                  onPanUpdate: (details) {
-                    controller.updatePosition(details);
-                  },
-                  onPanEnd: (details) {
-                    controller.movement.value = Offset(0, 0);
-                  },
-                  child: Obx(() => Container(
-                        width: controller.radius * 2,
-                        height: controller.radius * 2,
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          shape: BoxShape.circle,
-                        ),
-                        child: Center(
-                          child: Transform.translate(
-                            offset: controller.movement.value,
-                            child: Container(
-                              width: controller.radius,
-                              height: controller.radius,
+                  ),
+                  SizedBox(height: 24.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Obx(() => Text(
+                                '${controller.maxRotVel.value.toStringAsFixed(2)} rad/s',
+                                style: TextStyle(fontSize: 14),
+                              )),
+                          RotatedBox(
+                            quarterTurns: 3,
+                            child: Obx(() => Slider(
+                                  value: controller.maxRotVel.value,
+                                  min: 0,
+                                  max: 1.0,
+                                  divisions: 20,
+                                  onChanged: (value) {
+                                    controller.maxRotVel.value = value;
+                                  },
+                                )),
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: 16),
+                      GestureDetector(
+                        onPanStart: (details) {
+                          controller.center.value = details.localPosition;
+                          controller.movement.value = Offset(0, 0);
+                        },
+                        onPanUpdate: (details) {
+                          controller.updatePosition(details);
+                        },
+                        onPanEnd: (details) {
+                          controller.movement.value = Offset(0, 0);
+                        },
+                        child: Obx(() => Container(
+                              width: controller.radius * 2,
+                              height: controller.radius * 2,
                               decoration: BoxDecoration(
-                                color: Colors.blue,
+                                color: Colors.grey[200],
                                 shape: BoxShape.circle,
                               ),
-                            ),
+                              child: Center(
+                                child: Transform.translate(
+                                  offset: controller.movement.value,
+                                  child: Container(
+                                    width: controller.radius,
+                                    height: controller.radius,
+                                    decoration: BoxDecoration(
+                                      color: Colors.blue,
+                                      shape: BoxShape.circle,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )),
+                      ),
+                      SizedBox(width: 16),
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Obx(() => Text(
+                                '${controller.maxLinVel.value.toStringAsFixed(2)} m/s',
+                                style: TextStyle(fontSize: 14),
+                              )),
+                          RotatedBox(
+                            quarterTurns: 3,
+                            child: Obx(() => Slider(
+                                  value: controller.maxLinVel.value,
+                                  min: 0,
+                                  max: 1.0,
+                                  divisions: 20,
+                                  onChanged: (value) {
+                                    controller.maxLinVel.value = value;
+                                  },
+                                )),
                           ),
-                        ),
-                      )),
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Obx(() => Text(
-                          '${controller.maxLinVel.value.toStringAsFixed(2)} m/s',
-                          style: TextStyle(fontSize: 14),
-                        )),
-                    RotatedBox(
-                      quarterTurns: 3,
-                      child: Obx(() => Slider(
-                            value: controller.maxLinVel.value,
-                            min: 0,
-                            max: 1.0,
-                            divisions: 20,
-                            onChanged: (value) {
-                              controller.maxLinVel.value = value;
-                            },
-                          )),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 16.0),
-            Center(
-              child: ElevatedButton(
-                onPressed: resetValues,
-                child: Text('Reset Values'),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 24.0),
+                  ElevatedButton(
+                    onPressed: resetValues,
+                    child: Text('Reset Values'),
+                  ),
+                ],
               ),
             ),
           ],
--- ros_web_app/lib/ros_service.dart
+++ ros_web_app/lib/ros_service.dart
@@ -13,10 +13,10 @@ class RosService {
     try {
       channel = WebSocketChannel.connect(Uri.parse(uri));
       // 연결 후 상태 확인을 위한 ping 메시지 전송
-      var pingMessage = {
-        'op': 'ping'
-      };
-      channel!.sink.add(json.encode(pingMessage));
+      // var pingMessage = {
+      //   'op': 'ping'
+      // };
+      // channel!.sink.add(json.encode(pingMessage));
     } catch (e) {
       throw Exception('Failed to connect to ROS: $e');
     }
@@ -45,7 +45,6 @@ class RosService {
     channel?.stream.listen((message) {
       var data = json.decode(message);
       if (data['msg'] != null && data['msg']['data'] != null) {
-        // Base64 문자열을 디코딩하여 Uint8List로 변환
         Uint8List imageData = base64.decode(data['msg']['data']);
         int dataSize = imageData.length;
         callback(imageData, dataSize);
@@ -89,10 +88,9 @@ class RosService {
     };
     channel?.sink.add(json.encode(subscribeMessage));
     
-    // 명시적으로 한번 요청
     var requestMessage = {
       'op': 'call_service',
-      'service': '/static_map',  // static_map 서비스 사용
+      'service': '/static_map',  
       'type': 'nav_msgs/GetMap',
       'args': {}
     };
@@ -107,7 +105,6 @@ class RosService {
         var height = info['height'];
         var resolution = info['resolution'].toDouble();
         
-        // Convert base64 data to List<int>
         List<int> mapData = List<int>.from(msg['data']);
         
         callback(mapData, width, height, resolution);
